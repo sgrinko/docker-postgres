a5f309741f4064c8e84921e5085a990a2c9273fd
grufos <sergey.grinko@gmail.com>
2021-06-11 18:38:00 +0300

    убрал из db_target.sql создание начального задания

--- 12/db_target.sql
+++ 12/db_target.sql
@@ -10,13 +10,3 @@ ALTER DATABASE :"DB" OWNER TO deploy;
 GRANT ALL ON DATABASE :"DB" TO deploy;
 ALTER DATABASE :"DB" SET search_path = :DEV_SCHEMA, public, tiger;
 
-with _cmd as (
-    -- в 1-ю неделю месяца замораживаем идентификаторы транзакций, в остальные недели только собираем статистику
-    select 'vacuum JOB' as name, '0 0 * * 0' as schedule, 'do $$ begin if date_part(''day'', now()) <= 7 then perform dblink(''dblink_localserver'', ''VACUUM (FREEZE,ANALYZE);''); else perform dblink(''dblink_localserver'', ''VACUUM (ANALYZE);''); end if; end $$;' as command
-)
-select cron.schedule(_cmd.name, _cmd.schedule, _cmd.command)
-from _cmd
-   left join cron.job j on j.command = _cmd.command and j."database"=current_database()
-where j.command is null
-;
-
