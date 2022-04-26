7beaaeee146c90ff97dafb188d58a973cbda7340
sergey grinko <grufos@mail.ru>
2022-04-26 08:53:14 +0300

    Исправил пример запуска контейнера через docker run

--- 12/docker_start_12.sh
+++ 12/docker_start_12.sh
@@ -3,7 +3,7 @@ docker run --name my_postgres_12 --shm-size 2147483648 -p 5433:5432/tcp \
            -v /var/lib/pgsql/12_1/data:/var/lib/postgresql/data \
            -v /var/log/postgresql1:/var/log/postgresql \
            -v /mnt/pgbak2/:/mnt/pgbak \
-           -v /usr/share/postgres/tsearch_data:/usr/share/postgresql/tsearch_data
+           -v /usr/share/postgres/tsearch_data:/usr/share/postgresql/tsearch_data \
            -e POSTGRES_PASSWORD=postgres -e POSTGRES_HOST_AUTH_METHOD=trust -e DEPLOY_PASSWORD=postgres -e PGBOUNCER_PASSWORD=postgres -e TZ="Etc/UTC" \
            grufos/postgres:12.10 \
            -c shared_preload_libraries="plugin_debugger,pg_stat_statements,auto_explain,pg_buffercache,pg_cron,shared_ispell,pg_prewarm" -c shared_ispell.max_size=70MB
