5a9428acdae95684684b7cb35b8113a2a5cb0dd1
sergey grinko <grufos@mail.ru>
2022-06-22 19:17:03 +0300

    Выпуск версии 14.4 postgres. Решена проблема корректного завершения работы контейнера с postgres (во всех версиях)

--- 14/docker_start_14.sh
+++ 14/docker_start_14.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker run --rm --name my_postgres_14 --shm-size 2147483648 -p 5433:5432/tcp \
+docker run --rm --name my_postgres_14 --shm-size 2147483648 -p 5433:5432/tcp --stop-timeout 60 \
            -v /var/lib/pgsql/14_1/data:/var/lib/postgresql/data \
            -v /var/log/postgresql1:/var/log/postgresql \
            -v /mnt/pgbak2/:/mnt/pgbak \
            -v /usr/share/postgres/tsearch_data:/usr/share/postgresql/tsearch_data \
            -e POSTGRES_PASSWORD=postgres -e POSTGRES_HOST_AUTH_METHOD=trust -e DEPLOY_PASSWORD=postgres -e PGBOUNCER_PASSWORD=postgres -e TZ="Etc/UTC" \
-           grufos/postgres:14.3 \
-           -c shared_preload_libraries="plpgsql_check, plugin_debugger,pg_stat_statements,auto_explain,pg_buffercache,pg_cron,shared_ispell,pg_prewarm" -c shared_ispell.max_size=70MB
+           grufos/postgres:14.4 \
+           -c shared_preload_libraries="plpgsql_check,plugin_debugger,pg_stat_statements,auto_explain,pg_buffercache,pg_cron,shared_ispell,pg_prewarm" -c shared_ispell.max_size=70MB
