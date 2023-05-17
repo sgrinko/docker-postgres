ed8e673048c321736d9f5095ec790eeb8592a325
sergey grinko <grufos@mail.ru>
2023-05-17 11:40:25 +0300

    Переход на новые минорные версии всех контейнеров

--- 12/bin/docker_build.sh
+++ 12/bin/docker_build.sh
@@ -4,26 +4,62 @@ VERSION=12
 set -euo pipefail
 
 cd docker-pgbouncer
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgbouncer:latest
 cd ..
 
 cd docker-analyze
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_analyze:latest
 cd ..
 
 cd docker-mamonsu
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_mamonsu:latest
 cd ..
 
 cd docker-pgprobackup
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgprobackup_backup:latest
 cd ..
 
 cd docker-pgprorestore
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgprobackup_restore:latest
 cd ..
 
 cd docker-postgres
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_postgres:latest
 cd ..
 
--- 15/bin/docker_build.sh
+++ 15/bin/docker_build.sh
@@ -4,30 +4,72 @@ VERSION=15
 set -euo pipefail
 
 cd docker-pgbouncer
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgbouncer:latest
 cd ..
 
 cd docker-analyze
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_analyze:latest
 cd ..
 
 cd docker-mamonsu
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_mamonsu:latest
 cd ..
 
 cd docker-pgprobackup
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgprobackup_backup:latest
 cd ..
 
 cd docker-pgprorestore
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgprobackup_restore:latest
 cd ..
 
 cd docker-pgupgrade
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_pgupgrade:latest
 cd ..
 
 cd docker-postgres
+dir=`pwd`
+echo ""
+echo "====================================="
+echo " $dir"
+echo "====================================="
+echo ""
 docker build . -t ${VERSION}_postgres:latest
 cd ..
 
